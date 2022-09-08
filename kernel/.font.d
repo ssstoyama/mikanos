f18a61f6eb92df082ba3b854556a4fe3d18079a9
toyama <shouya.toyama@gmail.com>
2022-09-08 13:49:17 +0000

    Improve compile

---
+++ kernel/.font.d
@@ -0,0 +1,5 @@
+font.o: font.cpp font.hpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp frame_buffer_config.hpp
---
+++ kernel/.main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  frame_buffer_config.hpp graphics.hpp font.hpp
