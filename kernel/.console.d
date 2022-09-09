a29eb3ec59f0084a0ba8ab82139425b488c0c8ac
toyama <shouya.toyama@gmail.com>
2022-09-09 11:21:08 +0000

    Add console.cpp

---
+++ kernel/.console.d
@@ -0,0 +1,6 @@
+console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/string.h font.hpp \
+  /home/vscode/osbook/devenv/x86_64-elf/include/c++/v1/cstdint
