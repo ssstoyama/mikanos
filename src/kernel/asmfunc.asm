9121df97fa63a9275341fac60f3e441fb1dbc942
toyama <shouya.toyama@gmail.com>
2022-09-15 00:35:06 +0000

    Implement handler

--- src/kernel/asmfunc.asm
+++ src/kernel/asmfunc.asm
@@ -1,10 +1,7 @@
-; asmfunc.asm
-;
-; System V AMD64 Calling Convention
-; Registers: RDI, RSI, RDX, RCX, R8, R9
-
 bits 64
 section .text
+
+; Registeres: RDI, RSI, RDX, RCX, R8, R9
 
 global IoOut32  ; void IoOut32(uint16_t addr, uint32_t data);
 IoOut32:
@@ -17,4 +14,22 @@ global IoIn32  ; uint32_t IoIn32(uint16_t addr);
 IoIn32:
     mov dx, di    ; dx = addr
     in eax, dx
+    ret
+
+global GetCS ; uint64_t GetCS(void)
+GetCS:
+    xor eax, eax
+    mov ax, cs
+    ret
+
+global LoadIDT ; void LoadIDT(uint64_t limit, uint64_t offset)
+LoadIDT:
+    push rbp
+    mov rbp, rsp ; rbp = rsp, use rbp as base memory address
+    sub rsp, 10
+    mov [rsp], di ; limit
+    mov [rsp+2], rsi ; offset
+    lidt [rsp] ; Load Interrupt Descriptor Table Register
+    mov rsp, rbp
+    pop rbp
     ret
--- src/kernel/asmfunc.h
+++ src/kernel/asmfunc.h
@@ -3,6 +3,8 @@
 #include <stdint.h>
 
 extern "C" {
-  void IoOut32(uint16_t addr, uint32_t data);
-  uint32_t IoIn32(uint16_t addr);
+    void IoOut32(uint16_t addr, uint32_t data);
+    uint32_t IoIn32(uint16_t addr);
+    uint64_t GetCS(void);
+    void LoadIDT(uint64_t limit, uint64_t offset);
 }
