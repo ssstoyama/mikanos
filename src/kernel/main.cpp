8af7854445088b2b660b07f736a5e78267b7a29f
toyama <shouya.toyama@gmail.com>
2022-09-19 01:43:51 +0000

    Add measurement in printk

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -57,6 +57,12 @@ int printk(const char* format, ...) {
   result = vsprintf(s, format, ap);
   va_end(ap);
 
+  StartLAPICTimer();
+  console->PutString(s);
+  auto elapsed = LAPITimerElapsed();
+  StopLAPITimer();
+
+  sprintf(s, "[%9d]", elapsed);
   console->PutString(s);
   return result;
 }
